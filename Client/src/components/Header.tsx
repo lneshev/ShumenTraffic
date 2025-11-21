20651ab84a43e3f3cb5c0a3355ad5741df779496
Lyuboslav Neshev <l_neshev@abv.bg>
2025-11-21 20:28:48 +0200

    Change header's z-index to 1001, because the map overlap it

--- Client/src/components/Header.tsx
+++ Client/src/components/Header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
   ];
 
   return (
-    <header className="sticky top-0 z-50 bg-white dark:bg-slate-900 border-b border-gray-200 dark:border-slate-700 shadow-sm">
+      <header className="sticky top-0 z-1001 bg-white dark:bg-slate-900 border-b border-gray-200 dark:border-slate-700 shadow-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}

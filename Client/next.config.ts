8936ecfde74f95f9ae1d34dbede5821b8ece52c5
Lyuboslav Neshev <l_neshev@abv.bg>
2026-04-23 23:51:06 +0300

    Leave console logs, errors etc. in production

--- Client/next.config.ts
+++ Client/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone"
+  output: "standalone",
+  compiler: {
+    removeConsole: false
+  }
 };
 
 export default nextConfig;
